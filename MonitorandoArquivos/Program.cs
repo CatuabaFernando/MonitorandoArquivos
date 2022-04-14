a7f100f58b90b46b1faa0165934d0cc2e5979d15
Fernando Catuaba <fernandocatuaba1@gmail.com>
2022-04-14 17:13:21 -0300

    Monitorando arquivos.

---
+++ MonitorandoArquivos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+namespace NetCore_Monitor
+{
+    class Program
+    {
+        private static FileSystemWatcher _monitorar;
+        public static void MonitorarArquivos(string path, string filtro)
+        {
+            _monitorar = new FileSystemWatcher(path, filtro)
+            {
+                IncludeSubdirectories = true
+            };
+
+            _monitorar.Created += OnFileChanged;
+            _monitorar.Changed += OnFileChanged;
+            _monitorar.Deleted += OnFileChanged;
+            _monitorar.Renamed += OnFileRenamed;
+
+            _monitorar.EnableRaisingEvents = true;
+            Console.WriteLine($"Monitorando arquivos e: {filtro}");
+        }
+        private static void OnFileChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"O Arquivo {e.Name} {e.ChangeType}");
+        }
+        private static void OnFileRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine($"O Arquivo {e.OldName} {e.ChangeType} para {e.Name}");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Monitorando o sistema com : FileSystemWatcher");
+            string path = @"c:\dados";
+            string filtro = "*.txt";
+            MonitorarArquivos(path, filtro);
+            Console.ReadLine();
+        }
+    }
+}
